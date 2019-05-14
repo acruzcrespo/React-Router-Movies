69b1d7beabbe4a7f3c9a905c5572117428696c74
Ash <ashley.cruz.crespo@gmail.com>
2019-05-14 14:36:19 -0400

    working on the project

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
